6e4c962bf4d4691383344749f232e5a591e8a291
netero1981 <hakamatcity1980@gmail.com>
2026-01-17 21:14:04 +0100

    Create AzkarItem class with title and content

---
+++ app/src/main/java/com/azkari/AzkarItem.java
@@ -0,0 +1,20 @@
+package com.azkari;
+
+public class AzkarItem {
+
+    private String title;
+    private String content;
+
+    public AzkarItem(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
